ef21319e84853c15c99800f3624bb52d6605a3ec
Eduardo Achar <eduardoachar1717@gmail.com>
2023-09-05 11:32:33 -0300

    fiz no paint

--- Bots/BotZangado.py
+++ Bots/BotZangado.py
@@ -4,25 +4,38 @@ class BotZangado(Bot):
     def __init__(self,nome):
         self.__nome = nome
 
-    #nao esquecer o decorator
+    @property
     def nome(self):
-        pass
+        return self.__nome
 
-    #nao esquecer o decorator
-    def nome(nome):
-        pass
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
 
     def apresentacao(self):
-        pass
- 
+        return "OLÁ, EU SOU O BOT ZANGADO, PQ VOCÊ ESTÁ ME INCOMODANDO?"
+    
     def mostra_comandos(self):
-        pass
+        return "ME DIGA O QUE VOCE DESEJA: 1 - RECEBER UM BOAS-VINDAS, 2 - SABER ONDE EU MORO, 3 - SABER O MOTIVO DA MINHA EXISTENCIA, 4 - IR EMBORA(POR FAVOR ESCOLHA ESSE)"
     
     def executa_comando(self,cmd):
-        pass
+        if cmd == 1:
+            self.boas_vindas()
+        elif cmd == 2:
+            self.onde_moro()
+        elif cmd == 3:
+            self.minha_existencia()
+        elif cmd == 4:
+            self.despedida()
 
     def boas_vindas(self):
-        pass
+        return "SEJA MAL VINDO AO CHATBOT ZANGADO!"
+    
+    def onde_moro(self):
+        return "VOCÊ É MALUCO, EU NAO MORO EM LUGAR NENHUM, EU SOU UM ROBÔ"
+    
+    def minha_existencia(self):
+        return "olha... essa nem eu sei te responder... PORQUE SE FOR PRA CONVERSAR CONTIGO EU PREFIRO IR DE ALT + F4 LOGO"
 
     def despedida(self):
-        pass
+        return "ATÉ NUNCA MAIS!"
