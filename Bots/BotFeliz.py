b4834108b814e5415fa9505cacde37a2f2aa65fe
Erika <erikaamericano393@gmail.com>
2023-09-05 11:29:41 -0300

    add botfeliz e botfalador

---
+++ Bots/BotFeliz.py
@@ -0,0 +1,40 @@
+from Bots.Bot import Bot
+
+class BotFeliz(Bot):
+    def __init__(self, nome, comandos):
+        self.__nome = nome
+        self.__comandos = comandos
+        # self.comandos = {"O que é a vida?": "É uma maravilha!", "Comando 02": "Resposta 02", "Comando 03": "Resposta 03"}
+        super().__init__(nome, comandos)
+
+    #nao esquecer o decorator
+    def get_nome(self):
+        return self.__nome
+    
+    def set_nome(self, nome):
+        self.__nome = nome
+        
+    def get_comandos(self):
+        return self.__comandos
+
+    def set_comandos(self, comandos):
+        self.__comandos = comandos
+        
+    def apresentacao(self):
+        print("Olá, meu nome é BotFeliz da vida feliz.")
+ 
+    def mostra_comandos(self):
+        cmds_counter = 1
+        for cmds in self.comandos.keys():
+            print("{}- {}".format(cmds_counter, cmds_counter))
+            cmds_counter += 1            
+            
+    def executa_comando(self,cmd):
+        print("Você disse ---> {}".format(cmd))
+        print("Eu respondo ---> {}".format(self.comandos[cmd]))
+
+    def boas_vindas(self):
+        print("Bem-vindx ao meu ChatBot Feliz! :D")
+
+    def despedida(self):
+        print("Adeus e tenha uma boa vida!")
