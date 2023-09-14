8a14c201545a08ee9eeb47c19183e1c3b429ec87
Emanuelle <emanuguse@gmail.com>
2023-09-14 16:09:08 -0300

    erro de port

--- Bots/BotPrevisao.py
+++ Bots/BotPrevisao.py
@@ -6,11 +6,11 @@ class BotPrevisao(Bot):
     
     def __init__(self, nome):
         comandos = [Comando("Qual a previsão do tempo para hoje?", None),
-                    Comando("Suas previsões são confiáveis?", "Mostro uma média das temperaturas diárias, apeneas")]
+                    Comando("Suas previsões são confiáveis?", "Mostro uma média das temperaturas diárias, apenas")]
         super().__init__(nome, comandos)
         self.__api_key = "73ea9c3f03a0479a384487c43d0de627"
         
-    def descobre_previsao(self, cidade):
+    def descobre_previsao(self):
         cidade = input("Digite o nome da cidade: (0) ")
         url = "https://api.openweathermap.org/data/2.5/weather?q=sao+paulo&appid={}&lang=pt_br".format(self.__api_key)
         resposta = requests.get(url)
