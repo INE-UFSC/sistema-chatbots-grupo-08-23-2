049abcfaa19c45760a369d9219cacb13e4a1a5b9
Erika <erikaamericano393@gmail.com>
2023-10-19 13:52:02 -0300

    removendo codigo desnecessario - model/chatbot

--- MVC/model.py
+++ MVC/model.py
@@ -5,10 +5,7 @@ class Model:
         self.__bots = bots
         self.__nome_empresa = nome_empresa
         self.__bot_selecionado = bots[0] if len(bots) > 0 else None
-        if len(self.__bot_selecionado.comandos) > 0:
-            self.__comando_selecionado = self.__bot_selecionado.comandos[0]
-        else:
-            self.__comando_selecionado = None
+        self.__comando_selecionado = None
         self.__bot_dao = bot_dao
         self.__bot_dao.add("Nome da empresa" ,self.__nome_empresa)
         
